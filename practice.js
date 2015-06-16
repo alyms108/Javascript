3c1335f67ef3eb6c74da21bd3facd6684e2c96eb
alyms108 <amspratt89@gmail.com>
2015-06-16 09:43:45 -0500

    Finished up to 'Functions'
    Covered a bit more Javascript material, focusing on control flow (while
    and for loops) and functions.

--- practice.js
+++ practice.js
@@ -1,2 +1,35 @@
-var name = "John Doe";
-alert(name);
+// if(false){
+// 	alert("this is true.");
+// }else{
+// 	alert("no");
+// }
+// var x = 1;
+/*while(x < 10){
+	console.log(x);
+	x = x + 1;
+}*/
+
+/*for (var x = 1; x < 10; x++) {
+	console.log("Hello");
+	console.log("Number:" + x);
+};*/
+
+/*var arr = [];
+console.log(arr);
+arr.push("John");
+arr.push(3);
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+function sayHello(){
+	console.log("Hello there!");
+	console.log(15);
+	console.log(arr);
+}
+sayHello();*/
+
+var addition = function(a,b){
+	return a+b;
+}
+console.log(addition(3,4));
