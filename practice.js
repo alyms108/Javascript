fa181f992b806a0da0cfd29a502e4aa45e945c7a
alyms108 <amspratt89@gmail.com>
2015-06-17 08:22:16 -0500

    Completed up to 'Javascript Gets Classy'
    Covered mostly things relating to JSON and making objects and
    prototypes. Also dealt a little with DOM manipulation via Javascript.

--- practice.js
+++ practice.js
@@ -29,7 +29,35 @@ function sayHello(){
 }
 sayHello();*/
 
-var addition = function(a,b){
+/*var addition = function(a,b){
 	return a+b;
 }
-console.log(addition(3,4));
+console.log(addition(3,4));*/
+
+//Objects
+/*var user = {
+	first_name: "Joe",
+	last_name: "Smith",
+	sayHello: function(){console.log("Hello")}
+};
+console.log(user.first_name);
+user.sayHello();*/
+
+//Prototypes
+/*var User = function(first_name,last_name){
+	this.f_name = first_name;
+	this.l_name = last_name;
+}
+var me = new User('Anna','King');
+console.log(me.f_name);*/
+
+//Add function to User prototype on the fly
+/*User.prototype.sayHello = function(){
+	console.log("hi there!");
+}
+
+var sam = new User('Sam','Jorden');
+sam.sayHello();*/
+
+//Targeting HTML elements
+// var h = document.getElementById('main-heading');
